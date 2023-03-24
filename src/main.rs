06c324ed9458b772da3f454af656b9ac1cc6b13d
Cesar Cara <cesar.cara@protonmail.com>
2023-03-24 08:53:25 -0300

    first few lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, jqx!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+
+    dbg!(buffer.to_string());
+    Ok(())
 }
